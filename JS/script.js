34dd4e10672bb0b52772f8cf2628fd616499317e
Emanuele <emanuele65b@gmail.com>
2021-06-03 16:56:49 +0200

    add multipli di tre

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 // Lista numeri
 var i;
+
 for(i = 1; i < 101; i ++){
-    document.getElementById("demo").innerHTML += numero;
+    if (i % 3 == 0)
+    document.getElementById("demo").innerHTML += i + ' ' + 'Fizz' + '</br>';
 }
