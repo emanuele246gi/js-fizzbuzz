cf0d357b2049cb21d2ff4110b269a8ef5aef1fa2
Emanuele <emanuele65b@gmail.com>
2021-06-03 17:06:10 +0200

    add multipli di cinque

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,19 @@
 var i;
 
 for(i = 1; i < 101; i ++){
-    if (i % 3 == 0)
-    document.getElementById("demo").innerHTML += i + ' ' + 'Fizz' + '</br>';
+
+    if (i % 3 == 0){
+
+        document.getElementById("demo").innerHTML += i + ' ' + 'Fizz' + '</br>';
+
+    }
+    else if (i % 5 == 0){
+
+        document.getElementById("demo").innerHTML += i + ' ' + 'Buzz' + '</br>';
+
+    }
+    
+
+    
+
 }
