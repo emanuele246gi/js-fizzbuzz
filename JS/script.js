745744c546f3080866f4a714404c8f82e5015c70
Emanuele <emanuele65b@gmail.com>
2021-06-03 16:45:18 +0200

    fix inizio script

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,5 @@
 // Lista numeri
-var numero = ["1"];
-
-numero = 
-
-document.getElementById("demo").innerHTML += numero;
+var i;
+for(i = 1; i < 101; i ++){
+    document.getElementById("demo").innerHTML += numero;
+}
