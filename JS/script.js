f4ede3c2db1948c22489211a81de34579d6b7cc6
Emanuele <emanuele65b@gmail.com>
2021-06-03 17:15:53 +0200

    add entrambi multipli e resto

--- JS/script.js
+++ JS/script.js
@@ -13,8 +13,14 @@ for(i = 1; i < 101; i ++){
         document.getElementById("demo").innerHTML += i + ' ' + 'Buzz' + '</br>';
 
     }
-    
+    else if (i % 3 == 0 && i % 5 == 0){
 
+        document.getElementById("demo").innerHTML += i + ' ' + 'Fizz Buzz' + '</br>';
+
+    }
+    else {
+        document.getElementById('demo').innerHTML += i + '</br>';
+    }
     
 
 }
